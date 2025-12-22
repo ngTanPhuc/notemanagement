8966c585e95c70fa23149c72cdd401ca2d62b422
ngTanPhuc <nguyentanphuc020505@gmail.com>
2025-12-22 07:27:31 +0700

    create SecurityConfig

---
+++ src/main/java/com/ngtanphuc/notemanagement/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ngtanphuc.notemanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    // Inside your SecurityConfig class
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // Still allow registration
+                        .anyRequest().authenticated()               // Still lock private notes
+                )
+                // Add this to allow the default Spring Login Page
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+}
